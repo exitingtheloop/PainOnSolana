bf91f81b4528b780e4006a8b1d0b16eb610aa498
Christian Gabriel Cote <ccote@amberkinetics.com>
2024-07-26 14:31:01 -0700

    pain hero image update

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ function EdoTensei() {
 }
 
 function PainDialogue() {
-  return <img id="paindialogue" src="popjpegs/PEPEPAINDIALOGUE2.png" />;
+  return <img id="paindialogue" src="popjpegs/PEPEPAINDIALOGUE5.png" />;
 }
 
 function PainOnEdo() {
@@ -265,7 +265,7 @@ function SocialsHeader(props) {
           >
             <path
               d="M17.5 14H19C20.1046 14 21 13.1046 21 12V5C21 3.89543 20.1046 3 19 3H12C10.8954 3 10 3.89543 10 5V6.5M5 10H12C13.1046 10 14 10.8954 14 12V19C14 20.1046 13.1046 21 12 21H5C3.89543 21 3 20.1046 3 19V12C3 10.8954 3.89543 10 5 10Z"
-              stroke="#5800c4"
+              stroke="#b40000"
               strokeWidth="1.5"
               strokeLinecap="round"
               strokeLinejoin="round"
